ff264ff5930880b20f7b490a690b83f4ccd880d5
anoushka1111 <saha.anoushka@gmail.com>
2024-05-25 10:22:49 -0400

    main screen setup

---
+++ main.py
@@ -0,0 +1,13 @@
+#Anoushka Saha
+#Day 22 Project
+#Pong Arcade Game
+#May 25th, 2024
+
+from turtle import Turtle, Screen
+
+#Screen setup
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width = 800, height = 600)
+screen.title("PONG")
+screen.exitonclick()
