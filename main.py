f0e1d86061986e7bf73f7a594dec3a2d14602468
anoushka1111 <saha.anoushka@gmail.com>
2024-05-25 11:57:55 -0400

    created moving paddle

--- main.py
+++ main.py
@@ -7,7 +7,34 @@ from turtle import Turtle, Screen
 
 #Screen setup
 screen = Screen()
-screen.bgcolor("black")
 screen.setup(width = 800, height = 600)
+screen.bgcolor("black")
 screen.title("PONG")
+screen.tracer(0)
+
+#Paddle
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+    y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), y)
+
+def go_down():
+    y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_on = True
+while game_on:
+    screen.update()
+
+
 screen.exitonclick()
